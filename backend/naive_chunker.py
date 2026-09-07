0b17aad2782672432d022a55930d936bdb4acfc5
RengganaS <6182201071@student.unpar.ac.id>
2026-09-07 19:02:35 +0700

    Custom fixed-size

--- backend/naive_chunker.py
+++ backend/naive_chunker.py
@@ -1,30 +1,62 @@
-from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
+import pypdf
 
-def test_naive_chunking():
-    # hardcode file input
-    pdf_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'harries2015.pdf')
+def load_pdf_text(file_path):
+    """Loader Data PDF"""
+    extracted_text = ""
 
-    loader = UnstructuredPDFLoader(pdf_path)
-    docs = loader.load()
+    with open(file_path, 'rb') as file:
+        pdf_reader = pypdf.PdfReader(file)
 
-    # slicing text
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 5,
-        length_function= len
-    )
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
 
-    chunks = text_splitter.split_documents(docs)
+            extracted_text += page.extract_text() + " "
 
-    print(f"Document succesfully split into {len(chunks)} chunks.\n")
+    return extracted_text
 
-    # visualize output
-    for i, chunk in enumerate(chunks):
-        print(f"=== Chunk {i+1} (Length: {len(chunk.page_content)}) ===")
-        print(chunk.page_content)
-        print("\n" + "-"*50 + "\n")
+def fixed_size(text, chunk_size, overlap):
+    if overlap > chunk_size:
+        raise ValueError("Overlap must strictly less than chunk size.")
 
-if __name__ == "__main__":
-    test_naive_chunking()
+    words = text.split()
+    total_words = len(words)
+    stride = chunk_size - overlap
+    chunks = []
+
+    for i in range(0, total_words, stride):
+        chunk_slice = words[i : i + chunk_size]
+        chunk_text = " ".join(chunk_slice)
+        chunks.append(chunk_text)
+
+        if (i + chunk_size) >= total_words:
+            break
+
+    return chunks
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+pdf_path = os.path.join(current_dir, "..", "data", "harries2015.pdf")
+pdf_path = os.path.abspath(pdf_path)
+
+try:
+    print(f"Attempting to load: {pdf_path}...")
+    raw_document_string = load_pdf_text(pdf_path)
+
+    my_chunks = fixed_size(raw_document_string, chunk_size=200, overlap=40)
+
+    print(f"Successfully created {len(my_chunks)} chunks!\n")
+
+    for index, chunk in enumerate(my_chunks[:len(my_chunks)]):
+        print(f"--- Chunk {index + 1} (Words: {len(chunk.split())}) ---")
+        print(chunk)
+        print("-" * 40 + "\n")
+
+except FileNotFoundError:
+    print(f"Error: Could not find '{pdf_path}'.")
+    print("Ensure you have a folder named 'data' in the same directory as this script, containing 'sample.pdf'.")
+
+
+
+
+
+
